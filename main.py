a88d9a46417ac80bd9afdeb222c6dc58b694c630
Alex_Smirnov <95142946+R1ng3R@users.noreply.github.com>
2024-04-21 10:47:25 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,17 +8,20 @@ rf = Roboflow(api_key='lLbRrC8N05TcRCsxBSvW')
 project = rf.workspace().project('smoker_detection-c5aok')
 model = project.version(3).model
 
-for i in range(4):
-    a = str((model.predict('img/test/' + str(i+1) + '.jpg').json()))
-    print(Fore.WHITE + a)
+for i in range(500):
+    try:
+        a = str((model.predict('dataset/' + str(i+1) + '.jpg').json()))
+        print(Fore.WHITE + a)
 
-    if 'no_smoke' in a:
-        print(Fore.GREEN + 'не курит')
-        print('не курит с вероятностью', float(a[a.index("confidence': ") + 13:a.index("confidence': ") + 13 + 5]) * 100, '%')
-    elif 'smokers' in a:
-        print(Fore.RED + 'курит')
-        print('курит с вероятностью', float(a[a.index("confidence': ") + 13:a.index("confidence': ") + 13 + 5]) * 100, '%')
-    else:
-        print(Fore.YELLOW + 'не удалось определить')
+        if 'no_smoke' in a:
+            print(Fore.GREEN + 'не курит')
+            print('не курит с вероятностью', float(a[a.index("confidence': ") + 13:a.index("confidence': ") + 13 + 5]) * 100, '%')
+        elif 'smokers' in a:
+            print(Fore.RED + 'курит')
+            print('курит с вероятностью', float(a[a.index("confidence': ") + 13:a.index("confidence': ") + 13 + 5]) * 100, '%')
+        else:
+            print(Fore.YELLOW + 'не удалось определить')
+    except:
+        pass
 
 input()
